b58999d7b83da1d9ce4817966e663e7c8b732ebd
Jean Luc Nürrenberg <reallifejunkies@googlemail.com>
2019-04-23 17:01:27 +0200

    Oops

--- cogs/commands.py
+++ cogs/commands.py
@@ -12,28 +12,26 @@ def is_author():
         return True
     return commands.check(predicate)
 
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+
 class BecCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    def insert_returns(body):
-        # insert return stmt if the last expression is a expression statement
-        if isinstance(body[-1], ast.Expr):
-            body[-1] = ast.Return(body[-1].value)
-            ast.fix_missing_locations(body[-1])
-
-        # for if statements, we insert returns into the body and the orelse
-        if isinstance(body[-1], ast.If):
-            insert_returns(body[-1].body)
-            insert_returns(body[-1].orelse)
-
-        # for with blocks, again we insert returns into the body
-        if isinstance(body[-1], ast.With):
-            insert_returns(body[-1].body)
 
     @is_author()
     @commands.command()
-    async def exec(ctx, *, cmd):
+    async def exec(self, ctx, *, cmd):
         """Evaluates input.
         Input is interpreted as newline seperated statements.
         If the last statement is an expression, that is the return value.
